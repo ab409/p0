342c101a30affce50e881caec1d58bd0a76e6366
Yu guangyuan <guangyuanyu@sohu-inc.com>
2016-08-22 19:09:44 +0800

    a part

--- src/github.com/cmu440/p0/server_impl.go
+++ src/github.com/cmu440/p0/server_impl.go
@@ -2,28 +2,64 @@
 
 package p0
 
+import (
+    "net"
+    "bufio"
+    "fmt"
+    "strconv"
+)
+
 type multiEchoServer struct {
-	// TODO: implement this!
+    num int
 }
 
 // New creates and returns (but does not start) a new MultiEchoServer.
 func New() MultiEchoServer {
-	// TODO: implement this!
-	return nil
+    mes := multiEchoServer{num:0}
+    return mes;
 }
 
 func (mes *multiEchoServer) Start(port int) error {
-	// TODO: implement this!
-	return nil
+    if port > 65535 || port <= 0 {
+        fmt.Println("port is invalid");
+        panic("port is invalid");
+    }
+    listener, e := net.ListenTCP("tcp", ":" + port);
+    if e != nil {
+        panic("listen failed");
+    }
+    go func() {
+        for {
+            conn, err := listener.Accept();
+            if err != nil {
+                continue;
+            }
+            mes.num++;
+            go func() {
+                defer conn.Close();
+                var buf [512]byte;
+                for {
+                    len, e := conn.Read(buf);
+                    if e != nil {
+                        break;
+                    }
+                    if len > 0 {
+                        conn.Write(buf);
+                    }
+                }
+
+            }()
+        }
+    }()
+    return nil;
 }
 
 func (mes *multiEchoServer) Close() {
-	// TODO: implement this!
+    // TODO: implement this!
 }
 
 func (mes *multiEchoServer) Count() int {
-	// TODO: implement this!
-	return -1
+    return mes.num;
 }
 
 // TODO: add additional methods/functions below!
